4b895297703d66e10c1b79740c3c4a4c6b70a8e2
yym68686 <yym68686@outlook.com>
2022-11-15 09:44:34 +0800

    complete hashmaps

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -2,7 +2,7 @@
 
 // A list of scores (one per line) of a soccer match is given. Each line
 // is of the form :
-// <team_1_name>,<team_2_name>,<team_1_goals>,<team_2_goals>
+// <team_1_name>,<v[0].to_string()>,<team_1_goals>,<team_2_goals>
 // Example: England,France,4,2 (England scored 4 goals, France 2).
 
 // You have to build a scores table containing the name of the team, goals
@@ -14,7 +14,6 @@
 
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
@@ -25,6 +24,14 @@ struct Team {
     goals_conceded: u8,
 }
 
+fn build_Team(name: String, goals_scored: u8, goals_conceded: u8) -> Team {
+    Team {
+        name,
+        goals_scored,
+        goals_conceded,
+    }
+}
+
 fn build_scores_table(results: String) -> HashMap<String, Team> {
     // The name of the team is the key and its associated struct is the value.
     let mut scores: HashMap<String, Team> = HashMap::new();
@@ -32,14 +39,50 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
     for r in results.lines() {
         let v: Vec<&str> = r.split(',').collect();
         let team_1_name = v[0].to_string();
-        let team_1_score: u8 = v[2].parse().unwrap();
-        let team_2_name = v[1].to_string();
-        let team_2_score: u8 = v[3].parse().unwrap();
+        // let v[2].parse().unwrap(): u8 = v[2].parse().unwrap();
+        // let v[0].to_string() = v[1].to_string();
+        // let v[3].parse().unwrap(): u8 = v[3].parse().unwrap();
         // TODO: Populate the scores table with details extracted from the
         // current line. Keep in mind that goals scored by team_1
         // will be number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+        let team1 = build_Team (
+            v[0].to_string(),
+            v[2].parse().unwrap(),
+            v[3].parse().unwrap(),
+        );
+        let team11 = build_Team (
+            v[0].to_string(),
+            0,
+            0,
+        );
+        let team111 = build_Team (
+            v[0].to_string(),
+            v[2].parse().unwrap(),
+            v[3].parse().unwrap(),
+        );
+        let team2 = build_Team (
+            v[1].to_string(),
+            v[3].parse().unwrap(),
+            v[2].parse().unwrap(),
+        );
+        let team22 = build_Team (
+            v[1].to_string(),
+            0,
+            0,
+        );
+        let team222 = build_Team (
+            v[1].to_string(),
+            v[3].parse().unwrap(),
+            v[2].parse().unwrap(),
+        );
+        let count1 = scores.entry(team1.name).or_insert(team11);
+        (*count1).goals_scored += team111.goals_scored;
+        (*count1).goals_conceded += team111.goals_conceded;
+        let count2 = scores.entry(team2.name).or_insert(team22);
+        (*count2).goals_scored += team222.goals_scored;
+        (*count2).goals_conceded += team222.goals_conceded;
     }
     scores
 }
