bd8e6689453f54ee5a31fb62a9998eca70777d23
yym68686 <yym68686@outlook.com>
2022-12-04 10:21:20 +0800

    complete thread

--- exercises/threads/threads1.rs
+++ exercises/threads/threads1.rs
@@ -2,7 +2,6 @@
 // Execute `rustlings hint threads1` or use the `hint` watch subcommand for a hint.
 // This program should wait until all the spawned threads have finished before exiting.
 
-// I AM NOT DONE
 
 use std::thread;
 use std::time::Duration;
@@ -12,10 +11,11 @@ fn main() {
 
     let mut handles = vec![];
     for i in 0..10 {
-        thread::spawn(move || {
+        let handle = thread::spawn(move || {
             thread::sleep(Duration::from_millis(250));
             println!("thread {} is complete", i);
         });
+        handles.push(handle);
     }
 
     let mut completed_threads = 0;
@@ -27,5 +27,4 @@ fn main() {
     if completed_threads != 10 {
         panic!("Oh no! All the spawned threads did not finish!");
     }
-    
 }
