d24e7917c413750174d0781ac3a9c3916edd580c
yym68686 <yym68686@outlook.com>
2022-12-04 22:32:19 +0800

    complete All exercises completed!

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -35,10 +35,22 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        let s = s.split(',').collect::<Vec<&str>>();
+        if s.len() != 2 || s[0].len() == 0 {
+            return Person::default();
+        }
+        if let Ok(age) = s[1].to_string().parse::<usize>(){
+            Person{
+                name: String::from(s[0]),
+                age:age
+            }
+        }
+        else {
+            return Person::default();
+        }
     }
 }
 
